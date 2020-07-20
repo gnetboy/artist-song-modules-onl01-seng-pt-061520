5f54eb6ab527c27d244ec2585a187340ad284865
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-20 02:07:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  def initialize
-    @@songs << self
-  end
+ 
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
